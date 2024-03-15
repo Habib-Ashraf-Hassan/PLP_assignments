290c2d18aa4958554c79823c905d1ba581413b2a
Habib-Ashraf-Hassan <100794988+Habib-Ashraf-Hassan@users.noreply.github.com>
2024-03-15 16:00:40 +0300

    qn 1 and 2 done

--- week2_assignment.py
+++ week2_assignment.py
@@ -1,1 +1,12 @@
-#
+# creating empty list
+my_list = []
+
+# Append the following elements to my_list: 10, 20, 30, 40.
+item = 10
+for i in range(4):
+    my_list.append(item)
+    item += 10
+
+# check if elements were appended successfully
+print(my_list)
+
