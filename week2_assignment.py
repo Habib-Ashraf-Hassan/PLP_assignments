25ef07e2bdb00a1f174bd3938be1a938d73c2070
Habib-Ashraf-Hassan <100794988+Habib-Ashraf-Hassan@users.noreply.github.com>
2024-03-15 16:06:39 +0300

    all questions completed

--- week2_assignment.py
+++ week2_assignment.py
@@ -10,3 +10,20 @@ for i in range(4):
 # check if elements were appended successfully
 print(my_list)
 
+# Insert the value 15 at the second position in the list
+my_list.insert(1, 15)
+print(my_list)  # check
+
+# Extend my_list with another list: [50, 60, 70].
+my_list.extend([50, 60, 70])
+
+# Remove the last element from my_list.
+my_list.pop()
+
+# Sort my_list in ascending order.
+my_list.sort() 
+
+# Find and print the index of the value 30 in my_list.
+print(my_list.index(30))
+
+print(my_list)  # final check
